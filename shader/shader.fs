b1963e6405933ceb2c35018662f656acc7132e25
Yang <yangah9991@gmail.com>
2021-06-11 18:18:04 +0800

    Events to Shader

--- shader/shader.fs
+++ shader/shader.fs
@@ -10,8 +10,10 @@ uniform float ourColor;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
+uniform float mixVal;
+
 void main()
 {
     // FragColor = vec4(0,ourColor,0, 1.0);
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(-TexCoord.x, TexCoord.y)), mixVal);
 }
